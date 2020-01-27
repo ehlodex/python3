00ea8060bbbeabaa3b68b5a3c956dbf7d918ead4
Joshua Burkholder <ehlodex@users.noreply.github.com>
2020-01-27 15:00:42 -0500

    Update alarmclock.py

--- alarmclock.py
+++ alarmclock.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
+import os
+import time
 from datetime import datetime
-from time import sleep
+from playsound import playsound
 
-# TODO: Configure default path for alarm sounds
 # TODO: Allow custom alarm paths
-# TODO: Configure alarm_repeats a la cron
+# TODO: Configure alarm_repeat a la cron
 # TODO: Create an alarm object
-# TODO: Optimize alarm wait times, i.e. sleep()
+# TODO: Optimize alarm wait times, i.e. time.sleep()
 # TODO: Construct the pseudocode
 # TODO: Write the actual code
 
@@ -16,12 +17,14 @@ from time import sleep
 # alarms could (optionally) be saved into a separate file on the local disk for
 # preservation, or stored in a database (overkill). For now, starting with an
 # empty list, or a default list, is good enough for a rudimentary system.
-
-# The alarm_path should be a 'universal' or default location for alarm sounds.
-# Something like os.path.dirname(os.path.abspath(__file__)) could be used.
-alarm_path = 'C:\\ProgramData\\PylarmClock\\'
-sound_path = alarm_path + 'Sounds\\'
-sound_exts = ('.mp3', '.flac')
+alarm_path = os.path.dirname(os.path.abspath(__file__))
+sound_path = os.path.join(alarm_path, 'Sounds')
+sound_exts = ('.mp3', '.wav')
+default_alarm_time = '06:00'
+default_alarm_snooze = int(9)
+default_alarm_sound = os.path.join(sound_path, 'default.mp3')
+default_alarm_name = 'Alarm'
+default_alarm_repeat = '0 0 * * *'
 alarm_list = [
     {'alarm_time': '16:00', 'alarm_snooze': '5', 'alarm_active': True,
      'alarm_sound': 'exit_sound',
@@ -29,8 +32,8 @@ alarm_list = [
     {'alarm_time': '08:00', 'alarm_snooze': '9', 'alarm_active': True,
      'alarm_sound': 'startup_sound',
      'alarm_name': 'Sample Alarm 2'},
-    {'alarm_time': '13:37', 'alarm_snooze': '9', 'alarm_active': True,
-     'alarm_sound': 'C:\\Users\\Username\\Music\\Artist\\Album\\Song.mp3',
+    {'alarm_time': '13:32', 'alarm_snooze': '9', 'alarm_active': True,
+     'alarm_sound': r'C:\Users\Username\Music\Artist\Album\Song.mp3',
      'alarm_name': 'Sample Alarm 3'}
 ]
 
@@ -40,33 +43,65 @@ def main():
         now = datetime.now().strftime("%H:%M:%S")
         for i, alarm in enumerate(alarm_list):
             how_soon = alarm['alarm_time'] + ":00"
+            # calculate time delta
+            # If the delta is smaller, change the delta
             if how_soon == now:
                 print('The alarm \'{}\' is active!'.format(alarm['alarm_name']))
-                alarm_sound = alarm['alarm_sound']
-                # test alarm sound path
-                #   if it's a full path, use that
-                #   else see if it exists in sound_path, with or without exts
-                #   else use the default sound
+                alarm_sound = get_alarm_sound(alarm['alarm_sound'])
                 print('Playing the sound file at {}'.format(alarm_sound))
+                try:
+                    playsound(alarm_sound)
+                except:
+                    print('Cannot play any sounds! *beep* *beep* *beep*')
+                finally:
+                    # write the name and time of the next alarm, based on delta
+                    pass
                 # If snooze != 0, then prompt to snooze or cancel, presume that
                 # the user wants to snooze after 1 minute of inactivity.
-                # exit()
-        # Wait half of the time until the next alarm (e.g. if the next alarm
-        # sounds in 45 minutes, check the alarms again in 23 minutes). When the
-        # alarm will sound in less than 5 minutes, check every minute, until
-        # there is < 90 seconds remaining, then check every second.
-        sleep(5)
+                exit()
+        time.sleep(1)
 
-def set_alarm():
-    # args = alarm_name, alarm_time, alarm_snooze, alarm_sound
-    # set_alarm('Wakeup 1', '05:45', '9', 'destiny.mp3')
+
+def set_alarm(alarm_name, alarm_time, alarm_snooze, alarm_sound):
+    # create a new alarm
+    # add the alarm to alarm_list
     pass
 
 
-def delete_alarm():
-    # args = index_number or alarm_name
-    # delete_alarm(1) or delete_alarm('Wakeup 1')
+def ui_set_alarm():
+    # prompt for alarm values
+    # set_alarm(*args)
     pass
+
+
+def delete_alarm(alarm_id):
+    # if alarm_id is a string:
+    #     enumerate through alarm_list to find a match
+    #     set alarm_id to a number
+    # if alarm_id is a number:
+    #     remove it from the alarm_list
+    # else:
+    #     The alarm cannot be found
+    #     Print a list of available alarms
+    pass
+
+
+def ui_delete_alarm():
+    # print a list of alarm_name, alarm_time, alarm_repeat
+    # alarm_id = input('Which alarm would you like to delete? ')
+    # delete_alarm(alarm_id)
+    pass
+
+
+def get_alarm_sound(alarm_sound):
+    # assert os.path.isfile(globals()['default_alarm_sound'])
+    if os.path.isfile(alarm_sound): return alarm_sound
+    this_alarm = os.path.join(globals()['sound_path'], alarm_sound)
+    if os.path.isfile(this_alarm): return this_alarm
+    for ext in globals()['sound_exts']:
+        this_alarm_with_ext = this_alarm + '.' + ext
+        if os.path.isfile(this_alarm_with_ext): return this_alarm_with_ext
+    return globals()['default_alarm_sound']
 
 
 if __name__ == '__main__':
