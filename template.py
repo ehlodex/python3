0cbac00c635eceaedb219ffec78a5ff8d47792d2
Joshua Burkholder <ehlodex@users.noreply.github.com>
2020-01-27 10:02:22 -0500

    Create template.py

---
+++ template.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env/ python3
+"""Brief description of this file. Wrapped at 72 characters.
+
+PEP8 says that you should always use double-quote marks and end the
+docstring with the triple quote on its own line.
+"""
+
+# __all__ = []
+# __version__ = '0.1'
+# __author__ = 'ehlodex'
+
+# IMPORTS
+# Standard Libraries, Third-Party, Local
+# from module import function as alias
+
+
+# GLOBALS / CONSTANTS
+
+
+# FUNCTIONS
+def main():
+    pass
+
+
+# IF NAME MAIN
+if __name__ == '__main__':
+    main()
