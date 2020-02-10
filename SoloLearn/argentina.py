1b210990d2846c96413cac533deadfaa385b2e23
Joshua Burkholder <ehlodex@users.noreply.github.com>
2020-02-10 11:50:19 -0500

    Code Coach > Argentina

--- SoloLearn/argentina.py
+++ SoloLearn/argentina.py
@@ -2,7 +2,7 @@
 """SoloLearn > Code Coach > Argentina"""
 
 pesos = int(input('List price in pesos: '))
-cents = int(input('List price in dollars: ') * 100)
+cents = int(input('List price in dollars: ')) * 100
 
 # 2 cents / 1 peso
 pesos = pesos * 2
