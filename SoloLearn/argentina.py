a42a04b9e6c1b4bcea2f08e4f1771b0a60c3158d
Joshua Burkholder <ehlodex@users.noreply.github.com>
2020-02-10 11:37:06 -0500

    Code Coach > Argentina

---
+++ SoloLearn/argentina.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/ python3
+"""SoloLearn > Code Coach > Argentina"""
+
+pesos = int(input('List price in pesos: '))
+cents = int(input('List price in dollars: ') * 100)
+
+# 2 cents / 1 peso
+pesos = pesos * 2
+
+if pesos > cents:
+    print('Dollars')
+else:
+    print('Pesos')
