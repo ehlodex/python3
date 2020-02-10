3ed5b1a87814486bab4f97cc230fe8be05835fdb
Joshua Burkholder <ehlodex@users.noreply.github.com>
2020-02-10 11:39:13 -0500

    Code Coach > Gotham City

---
+++ SoloLearn/gothamcity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env/ python3
+"""SoloLearn > Code Coach > Gotham City"""
+
+criminals = int(input('How many baddies? '))
+
+if criminals < 5:
+    print('I got this!')
+elif criminals > 10:
+    print('Good Luck out there!')
+else:
+    print('Help me Batman')
