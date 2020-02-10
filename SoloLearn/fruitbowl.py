85f0120917149e48b77cb09cc7c726b0b80e5df5
Joshua Burkholder <ehlodex@users.noreply.github.com>
2020-02-10 11:15:31 -0500

    Code Coach > Fruit Bowl

---
+++ SoloLearn/fruitbowl.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env/ python3
+"""SoloLearn > Code Coach > Fruit Bowl"""
+
+fruit = int(input('How many pieces of fruit? '))
+apples = fruit / 2
+pies = int(apples // 3)  # integer division
+print(pies)
