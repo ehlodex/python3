11cec52b5646ea471d2366419325eb440c5c0257
Joshua Burkholder <ehlodex@users.noreply.github.com>
2020-02-10 11:48:48 -0500

    Code Coach > Hovercraft

---
+++ SoloLearn/hovercraft.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/ python3
+"""SoloLearn > Code Coach > Hovercraft"""
+
+sales = int(input('How many did you sell? ') * 3)
+expense = 21
+
+if sales > expense:
+    print('Profit')
+elif sales < expense:
+    print('Loss')
+else:
+    print('Broke Even')
