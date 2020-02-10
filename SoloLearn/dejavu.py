9f3a3eb51c5ce65cc43b18a23d862d2ae5cd37fd
Joshua Burkholder <ehlodex@users.noreply.github.com>
2020-02-10 10:40:48 -0500

    Create dejavu.py

---
+++ SoloLearn/dejavu.py
@@ -0,0 +1,15 @@
+/usr/bin/env/ python3
+"""SoloLearn > Code Coach > Deja Vu"""
+
+string = input()
+unique = True
+
+for letter in string:
+    count = string.count(letter)
+    if count > 1:
+        unique = False
+        
+if unique:
+    print('Unique')
+else:
+    print('Deja Vu')
