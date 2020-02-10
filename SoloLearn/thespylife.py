13e12e828c0d2251f6044cc2b59bb1147a1c3280
Joshua Burkholder <ehlodex@users.noreply.github.com>
2020-02-10 11:09:06 -0500

    Code Coach > The Spy Life

--- SoloLearn/thespylife.py
+++ SoloLearn/thespylife.py
@@ -1,9 +1,9 @@
-/usr/bin/env/ python3
+#!/usr/bin/env/ python3
 """SoloLearn > Code Coach > The Spy Life"""
 
 import re
 
-message = input()[::-1]
+message = str(input())[::-1]
 
-letters = re.compile('[^a-zA-Z]')
-print(letter.sub('', message)
+letters = re.compile('[^a-zA-Z ]')
+print(letters.sub('', message))
