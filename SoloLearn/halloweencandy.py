d8a6b2e59ef221da053e6cf73bd5c0e9667e3e59
ehlodex <ehlodex@users.noreply.github.com>
2020-05-06 10:00:20 -0400

    Rename pynet and sololearn

--- SoloLearn/halloweencandy.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""SoloLearn > Code Coach > Halloween Candy"""
-
-import math
-
-houses = int(input('houses: '))
-chance = lambda x : math.ceil((2 / x) * 100)
-print((chance(houses)))
---
+++ sololearn_temp/militarytime.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""SoloLearn > Code Coach > Military Time"""
+
+# You do NOT need datetime for Code Coach solution!
+
+# datetime is being used to display the current time as the default option for
+# the input prompt and is completely unnecessary (see last three lines of code)
+
+from datetime import datetime
+
+def convert_time(civ_time):
+    hhmm, ampm = civ_time.split(' ')
+    pm = True if ampm[0].lower() == 'p' else False
+    hh, mm = hhmm.split(':')
+    # The time is 'PM', but not 12:xx PM
+    if pm and int(hh) != 12:
+        hh = int(hh) + 12
+    # The time is '12:xx' AM
+    if not pm and int(hh) == 12:
+        hh = '00'
+    # The time is between '1:00 AM' and '9:59 AM'
+    if int(hh) < 10 and len(hh) < 2:
+        hh = '0' + str(hh)
+    mil_time = '{}:{}'.format(hh, mm)
+    return mil_time
+
+now = datetime.now().strftime('%I:%M %p')
+my_prompt = 'Enter a civilian time like "hh:mm AM/PM" [{}] '.format(now)
+print(convert_time(input(my_prompt) or now))
